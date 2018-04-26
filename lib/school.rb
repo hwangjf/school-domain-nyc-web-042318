9925763ecc23ca66cddecdf6bbde41bf3e84230f
Jeffrey Hwang <jeffreyhwang90@gmail.com>
2018-04-26 13:08:19 +0000

    automatically backed up by learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,17 @@
-# code here!
+class School
+  
+  def initialize(name)
+    @name = name
+    @roster = {}
+  end
+  
+  def roster
+    @roster
+  end
+  
+  def add_student(student, grade)
+    @roster[grade] = []
+    @roster[grade] << student
+  end
+  
+end
